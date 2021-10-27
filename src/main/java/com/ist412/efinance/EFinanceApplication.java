14b276bc811ad71865327892e89cdf6e12d84c4b
jcswanson <jcs6270@psu.edu>
2021-10-26 18:18:46 -0700

    initial commit

---
+++ src/main/java/com/ist412/efinance/EFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.ist412.efinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EFinanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EFinanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ist412/efinance/EFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ist412.efinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EFinanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
