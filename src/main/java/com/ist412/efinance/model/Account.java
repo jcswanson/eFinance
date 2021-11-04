82e174646947219bdb7d3e342ef108137d56bbaa
jordan <16jloudis@gmail.com>
2021-11-03 20:56:03 -0400

    Starting a new branch to work in

--- src/main/java/com/ist412/efinance/model/Account.java
+++ src/main/java/com/ist412/efinance/model/Account.java
@@ -1,4 +1,97 @@
 package com.ist412.efinance.model;
 
-public class Account {
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="accounts")
+public class Account implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "fName")
+    private String firstName;
+    @Column(name = "lName")
+    private String lastName;
+    @Column(name = "Email")
+    private String email;
+    @Column(name = "Pass")
+    private String password;
+    @Column(name = "User" )
+    private String user;
+    @Column(name = "Age")
+    private int age;
+    @Column(name = "SSN")
+    private int ssn;
+
+
+
+
+//    Getters and Setters
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(int ssn) {
+        this.ssn = ssn;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
 }
