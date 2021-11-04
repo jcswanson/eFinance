789d368cda945204ca2847c1180ead0e125a7db4
jordan <16jloudis@gmail.com>
2021-11-03 21:10:01 -0400

    added security dependency to POM file

--- src/main/java/com/ist412/efinance/repository/AccountRepository.java
+++ src/main/java/com/ist412/efinance/repository/AccountRepository.java
@@ -3,5 +3,5 @@ package com.ist412.efinance.repository;
 import com.ist412.efinance.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AccountRepository extends JpaRepository<Account, Long> {
-}
+//public interface AccountRepository extends JpaRepository<Account, Long> {
+//}
--- src/main/java/com/ist412/efinance/repository/LoanRepository.java
+++ src/main/java/com/ist412/efinance/repository/LoanRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.domain.AbstractAuditable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface LoanRepository extends JpaRepository<Loan, Long> {
-}
+//public interface LoanRepository extends JpaRepository<Loan, Long> {
+//
+//}
