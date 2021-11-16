98c42b1d5033dc3c40d99065968cdaedeba0bcdd
jcswanson <jcs6270@psu.edu>
2021-11-16 06:51:24 -0800

    more configuration, loans html, th fragments

--- src/main/java/com/ist412/efinance/repository/UserRepository.java
+++ src/main/java/com/ist412/efinance/repository/UserRepository.java
@@ -2,14 +2,15 @@ package com.ist412.efinance.repository;
 
 import com.ist412.efinance.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    @Override
-    Optional<User> findById(Long id);
+    @Query("SELECT u FROM User u WHERE u.username = ?1")
+    User findByUsername(String username);
 
 }
