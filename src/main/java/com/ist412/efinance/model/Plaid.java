883f75d1e31d7e20ac7b266cb0b8ad0ed49bafa2
jordan <16jloudis@gmail.com>
2021-12-23 15:01:29 -0500

    Changed some of the variables in Plaid class

--- src/main/java/com/ist412/efinance/model/Plaid.java
+++ src/main/java/com/ist412/efinance/model/Plaid.java
@@ -21,11 +21,6 @@ public class Plaid implements Serializable{
     @Column(name = "plaid_id")
     private long pid;
 
-    private static PlaidApi plaidClient;
-
-    private String CLIENT_ID = "61bf78bb9730990012e7d7eb";
-    private String SECRET = "912cf12d40964cdb8b5941eeebd660";
-
 
     public Plaid() {
 
@@ -35,46 +30,6 @@ public class Plaid implements Serializable{
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "plaidApplicant_id")
     private User plaidApplicant;
-
-
-
-//    public void handle(HttpExchange t) throws IOException {
-//        // Create your Plaid client
-//        HashMap<String, String> apiKeys = new HashMap<String, String>();
-//        apiKeys.put("clientId", CLIENT_ID);
-//        apiKeys.put("secret", SECRET);
-//        ApiClient apiClient = new ApiClient(apiKeys);
-//        apiClient.setPlaidAdapter(ApiClient.Sandbox);
-
-//        plaidClient = apiClient.createService(PlaidApi.class);
-
-//        User userFromDB = new User();
-//        String clientUserId = userFromDB.getUid().toString();
-//        LinkTokenCreateRequestUser user = new LinkTokenCreateRequestUser()
-//                .clientUserId(clientUserId);
-
-//        LinkTokenCreateRequest request = new LinkTokenCreateRequest()
-//                .user(user)
-//                .clientName("Plaid Test App")
-//                .products(Arrays.asList(Products.fromValue("auth")))
-//                .countryCodes(Arrays.asList(CountryCode.US))
-//                .language("en")
-//                .redirectUri("https://domainname.com/oauth-page.html")
-//                .webhook("https://webhook.example.com");
-//
-//        Response<LinkTokenCreateResponse> response = plaidClient
-//                .linkTokenCreate(request)
-//                .execute();
-//
-//                response.body();
-
-//    }
-
-
-
-
-
-
-
+    
 
 }
